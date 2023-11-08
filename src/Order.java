1e6eaecd9a8376dab0db4f5a74ed402bc1304e37
Eugene <evgenij.biryukov99@mail.ru>
2023-11-08 14:11:50 +0300

    Fix Order

--- src/Order.java
+++ src/Order.java
@@ -1,16 +1,16 @@
 public class Order {
-    private int count = 0;
+    private int orderCount = 0;
 
     public Order() {
 
     }
 
     public Order(int count) {
-        this.count = count;
+        this.orderCount = count;
     }
 
     @Override
     public String toString() {
-        return "Order has count: " + count;
+        return "Order has count: " + orderCount;
     }
 }
